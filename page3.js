a9f03aa600386f84f679e1a8b25c263e76cd6af0
Jesse !! <g00479739@atu.ie>
2025-12-07 17:16:29 +0000

    added images for page 3, tested page 3 js

---
+++ page3.js
@@ -0,0 +1,11 @@
+function rotateItems(choice)
+{
+    if (choice == 'left')
+    {
+        console.log("left button pressed");
+    }
+    else if (choice == 'right')
+    {
+        console.log("right button pressed");
+    }
+}
